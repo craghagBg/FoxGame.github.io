8d1a116ed07066944e2b24a545e39a2ecbd9a684
craghagBg <nqmasvobodniparoli@gmail.com>
2016-03-30 12:01:09 +0300

    add levels/levels.js

--- JSGame/game/game-project-javascript-master/js/gameApp.js
+++ JSGame/game/game-project-javascript-master/js/gameApp.js
@@ -3,25 +3,8 @@ var app = app || {};
 (function(app){
     function GameApp(){
         app.points = 0;
-        this.fox = app.hero(100, 100, 'imgs/foxes2.png');
-        this.gameObjects = [
-            app.obstacle(0, 10, 'imgs/bush.png'),
-            app.obstacle(0, 106, 'imgs/bush.png'),
-            app.obstacle(0, 202, 'imgs/bush.png'),
-            app.obstacle(0, 296, 'imgs/bush.png'),
-            app.obstacle(300, 10, 'imgs/bush.png'),
-            app.obstacle(300, 106, 'imgs/bush.png'),
-            app.obstacle(300, 296, 'imgs/bush.png'),
-            app.obstacle(540, 296, 'imgs/bush.png'),
-            app.obstacle(540, 10, 'imgs/bush.png'),
-            app.obstacle(540, 106, 'imgs/bush.png'),
-            app.obstacle(540, 202, 'imgs/bush.png'),
-            app.obstacle(540, 296, 'imgs/bush.png'),
-            app.obstacle(0, 296, 'imgs/bush.png'),
-            app.food(200, 296, 'imgs/grape.png'),
-            app.food(100, 296, 'imgs/grape.png'),
-            app.food(400, 96, 'imgs/grape.png'),
-            app.food(360, 350, 'imgs/grape.png')];
+        this.fox = app.level.hero;
+        this.gameObjects = app.level.objects;
     }
 
     GameApp.prototype.game = function (){
